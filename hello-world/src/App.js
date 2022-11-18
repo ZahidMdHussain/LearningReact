2c1bde532e1aa7df8b38a898797a4df72747c8e6
Zahid <zahimdhussain786@gmail.com>
2022-11-18 19:00:06 +0530

    Add files to Github

---
+++ hello-world/src/App.js
@@ -0,0 +1,12 @@
+
+const name = "React"
+function App() {
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>I started learning react JS.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ three-city/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+    <h1>City To Visit</h1>
+    <ul>
+      <li>Mumbai</li>
+      <li>Kolkata</li>
+      <li>Delhi</li>
+    </ul>
+    </>
+  )
+}
+
+export default App;
