33d7983c27cb8600f35f79b2b525592823d67fc6
Zahid <zahimdhussain786@gmail.com>
2022-11-20 18:56:17 +0530

    Add files to Github

---
+++ keeper-app/src/component/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import "./app.css"
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Note />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
