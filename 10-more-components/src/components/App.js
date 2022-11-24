876df0fc6ec0de42811cfcf06ca1c66f8d79fc81
Zahid <zahimdhussain786@gmail.com>
2022-11-24 21:33:32 +0530

    Add files to Github

--- 10-more-components/src/components/App.js
+++ 10-more-components/src/components/App.js
@@ -3,29 +3,27 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts.js";
 import Avatar from "./Avatar";
+
+function createCard(Contact){
+  return (<Card
+        key={Contact.id}
+        Key={Contact.id}
+        name={Contact.name}
+        img={Contact.imgURL}
+        tel={Contact.phone}
+        mail={Contact.email}
+      />);
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvsRmrBwU-CT2ZkHcSFapeBnfOg4ZhHh7Ykg&usqp=CAU" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        mail={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        mail={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        mail={contacts[2].email}
-      />
+      
+      {/* Map to loop through */}
+
+      {contacts.map(createCard)}
     </div>
   );
 }
