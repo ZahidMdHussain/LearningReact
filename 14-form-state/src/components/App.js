a98772a6e77cc13e26043881efbc77bce77b4303
Zahid <zahimdhussain786@gmail.com>
2022-11-28 17:11:28 +0530

    Add files to Github

---
+++ 14-form-state/src/components/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  function changeText(event) {
+    setName(event.target.value);
+  }
+  const [finalName, finalNameSet] = useState("");
+  function addChangeText(event) {
+    finalNameSet(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {finalName}</h1>
+      <form onSubmit={addChangeText}>
+        <input
+          onChange={changeText}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+
+export default App;
